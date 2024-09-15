296ed76efb4e5c9f2e855ef79abfc986e3dd214d
Luiz Zomer <lipezomer@gmail.com>
2024-09-15 13:01:05 -0300

    fix: cors problem in socket-io

--- backend/src/order/order.gateway.ts
+++ backend/src/order/order.gateway.ts
@@ -1,24 +1,32 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server, Socket } from "socket.io";
-import { CreateOrderDTO } from "./dto/create-order.dto";
-import { UsePipes, ValidationPipe } from "@nestjs/common";
-import { OrderService } from "./order.service";
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { CreateOrderDTO } from './dto/create-order.dto';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { OrderService } from './order.service';
 
-@WebSocketGateway({namespace: 'order'})
+@WebSocketGateway({
+  namespace: 'order',
+  cors: {
+    origin: '*',
+  },
+})
 export class OrderGateway {
-    constructor(
-        private readonly orderService: OrderService,
-    ){}
+  constructor(private readonly orderService: OrderService) {}
 
-    @WebSocketServer() server: Server
+  @WebSocketServer() server: Server;
 
-    @SubscribeMessage('newOrder')
-    @UsePipes(new ValidationPipe({ transform: true })) 
-    async handleNewOrder(@MessageBody() order: CreateOrderDTO){
-        await this.orderService.createOrder(order)
+  @SubscribeMessage('newOrder')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async handleNewOrder(@MessageBody() order: CreateOrderDTO) {
+    await this.orderService.createOrder(order);
 
-        const orderList = await this.orderService.FindAllOrder()
+    const orderList = await this.orderService.FindAllOrder();
 
-        this.server.emit('newOrderList', orderList)
-    }
+    this.server.emit('newOrderList', orderList);
+  }
 }
