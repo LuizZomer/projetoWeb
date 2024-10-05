cf79cde63cb71f4953b0e0ccc33e1f06aacb859b
Luiz Zomer <lipezomer@gmail.com>
2024-10-05 13:35:59 -0300

    fix: remove backend

--- backend/src/auth/dto/login-customer.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsString } from 'class-validator';
-
-export class loginCustomerDTO {
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  password: string;
-}
--- backend/src/auth/dto/login-user.dto copy.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class loginUserDTO {
-  @IsString()
-  username: string;
-
-  @IsString()
-  password: string;
-}
